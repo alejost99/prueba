681e384f50e681d6b288b163771537e105ce5b15
Alejandro Serna <alejoserna2014@gmail.com>
2020-06-22 14:19:00 -0500

    int

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+
+//Helpers
+require('./hbs/hbs')
+
+// Middlewars
+// app.use(express.static(__dirname + '/public'))
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+const port = process.env.PORT || 4000
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        name: 'aleJandro sErnA tEjadA'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+
+//Server
+app.listen(port, () => {
+    console.log(`Server on port ${port}`);
+})
